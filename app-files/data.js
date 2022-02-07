f0283348fc3cb172a4a1288518df7204090f3e19
kai-zhang0217 <kaizhang321@gmail.com>
2022-02-07 18:23:18 -0500

    adding finished project

---
+++ app-files/data.js
@@ -0,0 +1,628 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1_lobby",
+      "name": "Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.262593381866969,
+        "pitch": 0.01052441178623198,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8555613127245927,
+          "pitch": 0.15996965210809222,
+          "rotation": 0,
+          "target": "1-1a_-bernard_seeman_gallery"
+        },
+        {
+          "yaw": 0.9827134250607692,
+          "pitch": 0.2730079804310712,
+          "rotation": 0.7853981633974483,
+          "target": "2-1st-fl-hallway"
+        },
+        {
+          "yaw": 2.6860963572105394,
+          "pitch": 0.08986597735556323,
+          "rotation": 0.7853981633974483,
+          "target": "9-6_main_stage"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9353558031493883,
+          "pitch": 0.17180299112358455,
+          "title": "Grammy Award",
+          "gif": "<img src=\"img/Grammy_Award.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption":"National Academy of Recording Arts & Sciences 1958 Grammy to Louis Prima & Keely  Smith for best vocal group arrangement recording of \“That Old Black Magic\”."
+
+
+        },
+        {
+          "yaw": -2.0878748777307354,
+          "pitch": -0.024465596641807252,
+          "title": "Concert Gown",
+          "gif": "<img src=\"img/Concert_Gown.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model":'<iframe src="https://model-viewer1.glitch.me" type="text/html" width="245" height="300" frameborder="0" scrolling="no" allowfullscreen="true"> </iframe>',
+          "caption":"Stage gown worn by vocalist and actress Keely Smith circa 1955."
+
+        },
+        {
+          "yaw": -2.4545457394232137,
+          "pitch": 0.027604350405315614,
+          "title": "Fancy Fur",
+          "gif": "<img src=\"img/Fancy_Fur.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption": "Fur coat worn by vocalist and actress Keely Smith with personally monogrammed  interior circa 1955."
+        },
+
+        {
+          "yaw": 2.204787279933785,
+          "pitch": 0.11138883768780161,
+          "title": "Trotman Bass",
+          "gif": "<img src=\"img/Trotman_Bass.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption":"5/8 size round back German bass violin circa 1955. Owned by Jazz bassist Lloyd  Trotman. This instrument recorded Ben E. King’s classic “Stand By Me” in 1961."
+        },
+        {
+          "yaw": 1.943987094850736,
+          "pitch": 0.20969043211307792,
+          "title": "Bandstand 2",
+          "gif": "<img src=\"img/Bandstand_2.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption": "Tony Pastor Orchestra bandstand/music stand from the Hotel Pennsylvania, New York  City circa 1939."
+        },
+        {
+          "yaw": 2.0045177446220253,
+          "pitch": 0.0330062849106767,
+          "title": "Microphone",
+          "gif": "<img src=\"img/Mic.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption": "Shure Unidyne microphone model 55 circa 1940."
+        },
+        {
+          "yaw": -2.6723828587218907,
+          "pitch": 0.0840469866113569,
+          "title": "Bandstand 1",
+          "gif": "<img src=\"img/Bandstand_1.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption":"Buddy Rich Orchestra bandstand/music stand circa 1960."
+        },
+        {
+          "yaw": -2.966338875764512,
+          "pitch": -0.060744222877508136,
+          "title": "Dorsey TV",
+          "gif": "<img src=\"img/Dorsey_TV.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption":" 7\" Tele-Tone \"Porthole\" TV from 1949, This style is often referred to as the \"suitcase\" or  \"oscilloscope\" style. With brass plate on front \“Tommy Dorsey, 11-19-49, With Love  Janie\”."
+
+        },
+        {
+          "yaw": -3.085097362079196,
+          "pitch": -0.060623418578847676,
+          "title": "Award 1",
+          "gif": "<img src=\"img/Award_1.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption": "Chick Webb Memorial Award presented to Arthur Prysock as the outstanding male  singer for 1953 as tabulated by the Pittsburg Courier Poll."
+        },
+        {
+          "yaw": 1.6708944867832907,
+          "pitch": 0.07451255709407789,
+          "title": "Trombone",
+          "gif": "<img src=\"img/Trombone.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption": "1947 Conn 8H gold plated trombone owned by Ray Anderson. Used exclusively in  performance and recordings through 1993. "
+
+        },
+      ]
+    },
+    {
+      "id": "1-1a_-bernard_seeman_gallery",
+      "name": "Bernard Seeman Gallery",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.0288061768953742,
+        "pitch": 0.06894616914441443,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7098264027472716,
+          "pitch": 0.3994242843546445,
+          "rotation": 0.7853981633974483,
+          "target": "0-1_lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1st-fl-hallway",
+      "name": "First Floor Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.7179971065616693,
+        "pitch": 0.08160108581953196,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9013615483712787,
+          "pitch": 0.5584890356145067,
+          "rotation": 6.283185307179586,
+          "target": "3-2_hall_gallery"
+        },
+        {
+          "yaw": -0.5962494343887794,
+          "pitch": 0.4439266607123251,
+          "rotation": 0,
+          "target": "0-1_lobby"
+        },
+        {
+          "yaw": 2.7727027399171646,
+          "pitch": 0.31104071107319875,
+          "rotation": 0.7853981633974483,
+          "target": "4-3_milt_hinton_room"
+        },
+        {
+          "yaw": 1.9709904719431197,
+          "pitch": 0.19467004694698709,
+          "rotation": 0.7853981633974483,
+          "target": "7-5_teddy_charles_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2_hall_gallery",
+      "name": "Hall Gallery",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.464733322676171,
+        "pitch": 0.17300075384115132,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5586893846992993,
+          "pitch": 0.4541314722782559,
+          "rotation": 0,
+          "target": "2-1st-fl-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.013089340125454,
+          "pitch": 0.17030113075232656,
+          "title": "Travel Case",
+          "gif": "<img src=\"img/Travel_Case.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption": "Leather travel case for band music engraved on top “Louis Jordan Orchestra” circa  1940."
+        },
+        {
+          "yaw": 1.709783616342098,
+          "pitch": 0.021881517916627402,
+          "title": "Trumpet",
+          "gif": "<img src=\"img/Trumpet.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption":"Custom Martin Trumpet, burnished gold, serial number 205363 produced for Jazz  trumpeter Ernie Royal. Produced September 1959."
+        }
+      ]
+    },
+    {
+      "id": "4-3_milt_hinton_room",
+      "name": "Milt Hinton Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.4512950095698347,
+        "pitch": 0.06201375069421289,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.142104834651569,
+          "pitch": 0.10689934040158633,
+          "rotation": 0.7853981633974483,
+          "target": "7-5_teddy_charles_room"
+        },
+        {
+          "yaw": -2.096933315508929,
+          "pitch": 0.3746250841539336,
+          "rotation": 5.497787143782138,
+          "target": "2-1st-fl-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-4_coal_bin",
+      "name": "Coal Bin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6133294872832842,
+          "pitch": 0.06819937918248442,
+          "rotation": 0,
+          "target": "6-4a_wine_cellar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-4a_wine_cellar",
+      "name": "Wine Cellar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.1128391587425117,
+        "pitch": 0.015092877514536696,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1306084176668847,
+          "pitch": 0.22138213953181207,
+          "rotation": 0,
+          "target": "5-4_coal_bin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-5_teddy_charles_room",
+      "name": "Teddy Charles Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.6612207548956004,
+        "pitch": 0.12521839918387379,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8360120276986063,
+          "pitch": 0.22196210349097356,
+          "rotation": 5.497787143782138,
+          "target": "8-5a"
+        },
+        {
+          "yaw": -0.8744829163320276,
+          "pitch": 0.3226256182997602,
+          "rotation": 3.9269908169872414,
+          "target": "2-1st-fl-hallway"
+        },
+        {
+          "yaw": -2.6219201753832593,
+          "pitch": 0.22524410888749458,
+          "rotation": 0.7853981633974483,
+          "target": "9-6_main_stage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-5a",
+      "name": "Teddy Charles Room 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6214739077953162,
+          "pitch": 0.31260300704572863,
+          "rotation": 5.497787143782138,
+          "target": "7-5_teddy_charles_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-6_main_stage",
+      "name": "Main Stage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.1021371206353843,
+        "pitch": -0.038222339753410495,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.873960008027205,
+          "pitch": 0.04833712064759865,
+          "rotation": 0,
+          "target": "10-6a_library"
+        },
+        {
+          "yaw": 1.6740454028704015,
+          "pitch": 0.16611972685978493,
+          "rotation": 6.283185307179586,
+          "target": "7-5_teddy_charles_room"
+        },
+        {
+          "yaw": 2.5666261924332554,
+          "pitch": 0.0784796188068384,
+          "rotation": 0.7853981633974483,
+          "target": "0-1_lobby"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0404636453016618,
+          "pitch": 0.019630237611465873,
+          "title": "Sculpture",
+          "gif": "<img src=\"img/Sculpture.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption": "Roger Kramer sculpture “Man on Clarinet” black walnut, circa 2018."
+        }
+      ]
+    },
+    {
+      "id": "10-6a_library",
+      "name": "Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.560493247841647,
+        "pitch": -0.11619246960860785,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4479852457137241,
+          "pitch": 0.10009697274757201,
+          "rotation": 4.71238898038469,
+          "target": "9-6_main_stage"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0025262723684794,
+          "pitch": 0.028302497865514198,
+          "title": "Bass Saxphone",
+          "gif": "<img src=\"img/Bass_Saxophone.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption":"Beuscher “true tone” bass saxophone, serial number 164132 owned and played by  Wally Kane with the Sauter-Finnegan Orchestra as well as the Joe Raposo Sesame  Street band for over 40 years."
+        },
+        {
+          "yaw": -0.7480303883517223,
+          "pitch": -0.05930466903085119,
+          "title": "Award 2",
+          "gif": "<img src=\"img/Award_2.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption":"International Association of Jazz Educators Hall of Fame Award presented to Clem  DeRosa for his total dedication to jazz, jazz education and I.A.J.E. Presented at the  17th annual I.A.J.E. Conference, January 1990."
+
+        },
+        {
+          "yaw": 0.029026173627951124,
+          "pitch": 0.03235604096245126,
+          "title": "Award 3",
+          "gif": "<img src=\"img/Award_3.gif\" width=\"250px\" height=\"auto\">",
+          "threed_model": "",
+          "caption": "Lifetime Achievement Award presented to Bucky Pizzarelli for his contribution to Jazz  guitar worldwide at the North Wales International Jazz Guitar Festival, 2005."
+        }
+      ]
+    }
+  ],
+  "name": "TheJazzLoft",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
